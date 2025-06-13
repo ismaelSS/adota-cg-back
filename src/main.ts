88b0f203846ec27d666534cfb547fc0e9ad2b487
ismaelss <ismael1337sn@gmail.com>
2025-06-13 18:35:56 -0300

    add cors url variable

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     new ValidationPipe({transform: true, transformOptions:{groups: ['transform']}}),
   );
     app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.ORIGIN_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
